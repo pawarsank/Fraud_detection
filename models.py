d3c52cf930860b95dc4903e8c11e190040f78a55
pawarsank <sanketplus@yahoo.co.in>
2022-07-30 00:01:34 +0530

    added models

--- models.py
+++ models.py
@@ -1,1 +1,10 @@
 print('adding all machine learning models.')
+
+def model1():
+    print ("code for model1")
+
+def model2():
+    print ("code for model2")
+
+def model3():
+    print ("code for model3")
